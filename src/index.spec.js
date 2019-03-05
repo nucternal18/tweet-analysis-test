172f09d3bc259d465e4a79cafb3f9e82d3647f02
Alex Brown <alexanderbrown.info@gmail.com>
2019-03-05 16:08:18 +0000

    make the first test fail

--- src/index.spec.js
+++ src/index.spec.js
@@ -2,7 +2,5 @@ import data from '../mocks'
 import { getTweetsBySearchString } from './'
 
 describe('getTweetsBySearchString', () => {
-  it('returns an array of tweets whose text matches the provided search string', () => {
 
-  })
 })
