15d6ab76a130f0f70acc52be6d992878f25edceb
nucternal18 <39033367+nucternal18@users.noreply.github.com>
2019-03-09 20:43:35 +0000

    Created requested function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-export const getTweetsBySearchString = () => {}
+//const data = require('../mocks')
+export const getTweetsBySearchString = (str, str2) => { 
+    let myArr = [];
+    let tot = [];
+    let myArr2 = [];
+    let myHashStr = [];
+    
+
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].text.includes(str)){
+            myArr.push(data[i].text)
+        }
+
+    };
+
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].text.includes(str2)){
+            myArr2.push(data[i].text)
+        }
+    };
+    const myArr3 = myArr2.toString().split(' ');
+    for (let i = 0; i < myArr3.length; i++){
+        if (myArr3[i].includes(str2)) {
+            myHashStr.push(myArr3[i])
+        }
+    
+    }
+
+    for (let i = 0; i < data.length; i++) {
+        tot.push(data[i].likes)
+    
+    }
+
+    const arrSum = arr => arr.reduce((a,b) => a + b, 0)
+
+    return [myArr, myHashStr, arrSum(tot)]
+    
+
+
+};
+
+
+
